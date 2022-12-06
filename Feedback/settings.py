41dbc239c838d071f4513565c0494781ea1943a2
rohit agrawal <rohitagrawal7201@gmail.com>
2022-12-06 11:55:10 +0530

    Added statics

--- Feedback/settings.py
+++ Feedback/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-p@^rlq)*qkwrbdunmignmllt%g^f$6*fw0b-era)mn+j^p2$#1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
